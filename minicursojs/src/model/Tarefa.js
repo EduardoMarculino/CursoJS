4ee32d1d891c2417f00753560e74435a1cb71f77
EduardoMarculino <44325581+EduardoMarculino@users.noreply.github.com>
2018-11-13 17:30:44 -0300

    Add files via upload

---
+++ minicursojs/src/model/Tarefa.js
@@ -0,0 +1,34 @@
+class Tarefa {
+	constructor(texto, id){
+		this._id = id;
+		this._texto = texto;
+		this._status = false;
+	}
+
+	get texto(){
+		return this._texto;
+	}
+
+	set texto(texto){
+		this._texto = texto;
+	}
+
+	get id(){
+		return this._id;
+	}
+
+	set id(id){
+
+		this._id = id;
+
+	}	
+
+	get status(){
+		return this._status;
+	}
+
+	set status(status){
+		this._status = status;
+	}
+
+}
